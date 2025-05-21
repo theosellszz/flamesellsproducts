a4054c2361a273c1e9fb0cd820677f573abb048b
theosellszz <ted914063@gmail.com>
2025-05-21 17:04:31 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Welcome to Resell Store</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
