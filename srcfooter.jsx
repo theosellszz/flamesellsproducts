a2a8233414c057d8fb542b0bd800118f6f7d6d8f
theosellszz <ted914063@gmail.com>
2025-05-21 17:18:06 -0400

    Create srcfooter.jsx

---
+++ srcfooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-center p-4 text-white bg-black bg-opacity-70">
+      <p>&copy; 2025 The Resell Plug</p>
+    </footer>
+  )
+}
+
+export default Footer
