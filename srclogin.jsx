fb5c070cf6c7501e6889bdc1447b614cf76559e2
theosellszz <ted914063@gmail.com>
2025-05-21 17:19:11 -0400

    Create srclogin.jsx

---
+++ srclogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className="text-white flex flex-col items-center justify-center min-h-screen pt-32">
+      <h2 className="text-3xl font-bold mb-4">Login</h2>
+      <p>Signup coming soon. For now, message us to create an account manually.</p>
+    </div>
+  )
+}
+
+export default Login
